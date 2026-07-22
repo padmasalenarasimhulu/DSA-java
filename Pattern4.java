f95363df83521992f7170a52c493235966d1d11c
P.Narasimhulu <padmasalenarasimha@gmail.com>
2026-07-22 14:58:00 +0530

    Daimond Pattern

---
+++ Pattern4.java
@@ -0,0 +1,29 @@
+public class Pattern4 {
+    public static void main(String[] args){
+        for(int i=1 ; i<=5; i++){
+            for(int j=1; j<=5-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=2*i-1; k++){
+                System.out.print("*");
+            }
+            for(int r=1; r<=5-i; r++){
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+        for(int i=5; i>=1; i--){
+            for(int j=1; j<=5-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=2*i-1; k++){
+                System.out.print("*");
+            }
+            for(int r=1; r<=5-i; r++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
