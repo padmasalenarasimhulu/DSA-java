dd4fde3ac5a7c6d44c2c5f43686c522ab489e1cb
P.Narasimhulu <padmasalenarasimha@gmail.com>
2026-07-24 12:33:18 +0530

    Solid Square Pattern

---
+++ SolidSquare.java
@@ -0,0 +1,12 @@
+public class SolidSquare {
+    public static void main(String[] args){
+        int Size = 5;
+        for(int i = 1; i < Size; i++){
+            for(int j = 1; j < Size; j++){
+                System.out.print("*" + " ");
+
+            }
+            System.out.println();
+        }
+    }
+}
